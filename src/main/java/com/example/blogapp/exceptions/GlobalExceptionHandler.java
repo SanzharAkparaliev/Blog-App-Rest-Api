caeb09dfce22560281d6ded71cf42dd6aaed8dca
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-04-17 17:42:44 +0600

    user id not found handling exception

---
+++ src/main/java/com/example/blogapp/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.blogapp.exceptions;
+
+import com.example.blogapp.payloads.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourseNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourseNotFoundExceptionHandler(ResourseNotFoundException ex){
+        String message = ex.getMessage();
+        ApiResponse apiResponse = new ApiResponse(message,false);
+        return  new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+}
